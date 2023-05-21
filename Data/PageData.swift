4f057267426e8f89886d1132aa1a258e88cbc112
JericFederer <pianocoderodyssey@gmail.com>
2023-05-22 08:52:44 +0900

    first commit

---
+++ Data/PageData.swift
@@ -0,0 +1,13 @@
+//
+//  PageData.swift
+//  Pinch
+//
+//  Created by JericFederer97 on 2023/04/07.
+//
+
+import Foundation
+
+let pagesData: [Page] = [
+  Page(id: 1, imageName: "magazine-front-cover"),
+  Page(id: 2, imageName: "magazine-back-cover")
+]
---
+++ PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by JericFederer97 on 2022/09/07.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
